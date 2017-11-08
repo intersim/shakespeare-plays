152bb3adf92fe5db7bcf4446b826334258ad9426
Emily Intersimone <emily.intersimone@gmail.com>
2017-11-08 18:03:24 -0500

    starting to create main export object

---
+++ index.js
@@ -0,0 +1,47 @@
+const listOfPlays = require('./data/toc.json')
+
+/*
+  each play should look like:
+  {
+    overview: ...,
+    act_1: {
+      scene_1: [...], // array of line objects
+      scene_2: [...]
+    },
+    act_2: ...,
+    act_3: ...,
+    act_4: ...,
+    act_5: ...,
+    complete: ...
+  }
+*/
+
+function makePlayObject (playName) {
+  const playObj = {};
+  const basePath = `./data/plays/${playName}`
+
+  const overview = require(`${basePath}/${playName}_overview.json`);
+  playObj.overview = overview;
+  
+  const complete = require(`${basePath}/${playName}_complete.json`);
+  playObj.complete = complete;
+
+  [1, 2, 3, 4, 5].forEach(actNum => {
+    const sceneNums = overview.acts[actNum];
+    playObj[`act_${actNum}`] = {};
+
+    sceneNums.forEach(sceneNum => {
+      playObj[`act_${actNum}`][`scene_${sceneNum}`] = require(`${basePath}/act_${actNum}/scene_${sceneNum}.json`);
+    });
+  })
+
+  return playObj;
+};
+
+makePlayObject('Macbeth');
+
+listOfPlays.forEach(play => {
+  
+});
+
+module.exports = {};
